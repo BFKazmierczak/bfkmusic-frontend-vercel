7a0bb51625cfee94b85194aa0dfd9165851fa415
BFKazmierczak <bfkazmierczak@gmail.com>
2024-03-11 09:11:19 +0100

    fix: remove redundant ToastContainer

--- src/components/_debug/DebugButton.tsx
+++ src/components/_debug/DebugButton.tsx
@@ -19,20 +19,6 @@ const DebugButton = () => {
         onClick={() => throwAnError()}>
         Wywołaj błąd
       </button>
-
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition={Bounce}
-      />
     </div>
   )
 }
